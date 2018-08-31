1c23e27c4689fe929298bb06385de154e3f007d6
Samuel Wang <samuelwhx@gmail.com>
2018-08-31 13:02:33 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './components/add/add.component';
+import { ListComponent } from './components/list/list.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+
+//standard material modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+//moment module for date-time
+import { MatMomentDateModule	} from '@angular/material-moment-adapter';
+
+//flex module for css alignment
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+const MODULES = [
+  //standard material modules
+  MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatDatepickerModule,
+  MatButtonModule,
+  MatIconModule,
+  //moment
+  MatMomentDateModule,
+  //flex
+  FlexLayoutModule
+];
+
+@NgModule({
+  imports: MODULES,
+  exports: MODULES
+})
+export class MaterialModule { }
